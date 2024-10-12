6412eb807ebc6a8d6632dc540674c31f930c3b92
lexaeliseev <lexaeliseev@gmail.com>
2024-10-12 16:46:07 +0300

    add print_name_func_and_args

--- test_homework.py
+++ test_homework.py
@@ -68,33 +68,40 @@ def test_find_suitable_user():
     ]
 
 
-# # Сделайте функцию, которая будет печатать
-# # читаемое имя переданной ей функции и значений аргументов.
-# # Вызовите ее внутри функций, описанных ниже
-# # Подсказка: Имя функции можно получить с помощью func.__name__
-# # Например, вызов следующей функции должен преобразовать имя функции
-# # в более читаемый вариант (заменить символ подчеркивания на пробел,
-# # сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
-# # >>> open_browser(browser_name="Chrome")
-# # "Open Browser [Chrome]"
-#
-#
-# def test_readable_function():
-#     open_browser(browser_name="Chrome")
-#     go_to_companyname_homepage(page_url="https://companyname.com")
-#     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
-#
-#
-# def open_browser(browser_name):
-#     actual_result = None
-#     assert actual_result == "Open Browser [Chrome]"
-#
-#
-# def go_to_companyname_homepage(page_url):
-#     actual_result = None
-#     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
-#
-#
-# def find_registration_button_on_login_page(page_url, button_text):
-#     actual_result = None
-#     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+# Сделайте функцию, которая будет печатать
+# читаемое имя переданной ей функции и значений аргументов.
+# Вызовите ее внутри функций, описанных ниже
+# Подсказка: Имя функции можно получить с помощью func.__name__
+# Например, вызов следующей функции должен преобразовать имя функции
+# в более читаемый вариант (заменить символ подчеркивания на пробел,
+# сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
+# >>> open_browser(browser_name="Chrome")
+# "Open Browser [Chrome]"
+
+def print_name_func_and_args(func, *args):
+    name_func = func.__name__.title().replace("_", " ")
+    name_args = "| ".join([*args])
+    result = name_func + " [" + name_args + "]"
+    print(result)
+    return result
+
+
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+
+
+def open_browser(browser_name):
+    actual_result = print_name_func_and_args(open_browser, "Chrome")
+    assert actual_result == "Open Browser [Chrome]"
+
+
+def go_to_companyname_homepage(page_url):
+    actual_result = print_name_func_and_args(go_to_companyname_homepage, "https://companyname.com")
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = print_name_func_and_args(find_registration_button_on_login_page, "https://companyname.com/login, Register")
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
