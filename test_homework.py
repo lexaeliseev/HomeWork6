58781338dc87006a89cb6b585171c92f6d7f8392
lexaeliseev <lexaeliseev@gmail.com>
2024-10-12 13:46:42 +0300

    add example test_dark_theme_by_time_and_user_choice

--- test_homework.py
+++ test_homework.py
@@ -14,23 +14,30 @@ def test_dark_theme_by_time():
     assert is_dark_theme is True
 
 
-# def test_dark_theme_by_time_and_user_choice():
-#     """
-#     Протестируйте правильность переключения темной темы на сайте
-#     в зависимости от времени и выбора пользователя
-#     dark_theme_enabled_by_user = True - Темная тема включена
-#     dark_theme_enabled_by_user = False - Темная тема выключена
-#     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
-#     """
-#     current_time = time(hour=16)
-#     dark_theme_enabled_by_user = True
-#     # TODO переключите темную тему в зависимости от времени суток,
-#     #  но учтите что темная тема может быть включена вручную
-#
-#     is_dark_theme = None
-#     assert is_dark_theme is True
-#
-#
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+    # TODO переключите темную тему в зависимости от времени суток,
+    #  но учтите что темная тема может быть включена вручную
+    if dark_theme_enabled_by_user is True:
+        is_dark_theme = True
+    elif dark_theme_enabled_by_user is False:
+        is_dark_theme = False
+    else:
+        if time(hour=22) < current_time or current_time < time(hour=6):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+
+    assert is_dark_theme is True
+
 # def test_find_suitable_user():
 #     """
 #     Найдите нужного пользователя по условиям в списке пользователей
