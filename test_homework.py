940979ca87b86a24a27efcb442f1d24495776363
lexaeliseev <lexaeliseev@gmail.com>
2024-10-12 14:41:35 +0300

    add test_find_suitable_user

--- test_homework.py
+++ test_homework.py
@@ -35,33 +35,39 @@ def test_dark_theme_by_time_and_user_choice():
             is_dark_theme = True
         else:
             is_dark_theme = False
-
     assert is_dark_theme is True
 
-# def test_find_suitable_user():
-#     """
-#     Найдите нужного пользователя по условиям в списке пользователей
-#     """
-#     users = [
-#         {"name": "Oleg", "age": 32},
-#         {"name": "Sergey", "age": 24},
-#         {"name": "Stanislav", "age": 15},
-#         {"name": "Olga", "age": 45},
-#         {"name": "Maria", "age": 18},
-#     ]
-#
-#     # TODO найдите пользователя с именем "Olga"
-#     suitable_users = None
-#     assert suitable_users == {"name": "Olga", "age": 45}
-#
-#     # TODO найдите всех пользователей младше 20 лет
-#     suitable_users = None
-#     assert suitable_users == [
-#         {"name": "Stanislav", "age": 15},
-#         {"name": "Maria", "age": 18},
-#     ]
-#
-#
+
+def test_find_suitable_user():
+    """
+    Найдите нужного пользователя по условиям в списке пользователей
+    """
+    users = [
+        {"name": "Oleg", "age": 32},
+        {"name": "Sergey", "age": 24},
+        {"name": "Stanislav", "age": 15},
+        {"name": "Olga", "age": 45},
+        {"name": "Maria", "age": 18},
+    ]
+
+    # TODO найдите пользователя с именем "Olga"
+    suitable_users = None
+    for user in users:
+        if user["name"] == "Olga":
+            suitable_users = user
+    assert suitable_users == {"name": "Olga", "age": 45}
+
+    # TODO найдите всех пользователей младше 20 лет
+    suitable_users = []
+    for user in users:
+        if user["age"] < 20:
+            suitable_users.append(user)
+    assert suitable_users == [
+        {"name": "Stanislav", "age": 15},
+        {"name": "Maria", "age": 18},
+    ]
+
+
 # # Сделайте функцию, которая будет печатать
 # # читаемое имя переданной ей функции и значений аргументов.
 # # Вызовите ее внутри функций, описанных ниже
